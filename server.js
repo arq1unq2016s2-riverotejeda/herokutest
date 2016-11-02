5b8b04f5053550762fc31c719ef9be46ca90f18d
Mar <tejedamarcia@gmail.com>
2016-11-01 23:33:22 -0300

    server.js

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(express.static(__dirname + '/app'));
 app.get('/', function(req, res) {
 
     // make sure index is in the right directory. In this case /app/index.html
-    res.render('index');
+//    res.render('index');
+	res.render('/app/index.html');
 });
 
 app.listen(port, function() {
